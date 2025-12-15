00e5ef9b192fcf1f28456182674fb3bc717be1e1
Md. Mostak Shahid <162551046+mostakprime@users.noreply.github.com>
2025-12-15 17:14:10 +0600

    Worked on Box shadow and Text Shadow

--- src/components/BoxShadowControl/BoxShadowControl.jsx
+++ src/components/BoxShadowControl/BoxShadowControl.jsx
@@ -20,66 +20,62 @@ const BoxShadowControl = ({ value = {}, onChange, className='' }) => {
 
     return (
         <div className={`box-shadow-wrapper ${className}`}>
-            <div className="d-flex justify-content-end mb-2">
-                <Space align='center'>
-                    <Switch 
-                        aria-label={__('Enable Box Shadow', 'plugin-starter')}
-                        checked={!!shadow.enabled}
-                        onChange={(enabled) => update('enabled', enabled)}
-                    />
-                    <Typography.Title heading={6} style={{ margin: 8 }}>
-                        {shadow.enabled ? __('Enabled', 'plugin-starter') : __('Disabled', 'plugin-starter')}
-                    </Typography.Title>
-                </Space>
-            </div> 
+            <Space align='center'>
+                <Switch 
+                    aria-label={__('Enable Box Shadow', 'plugin-starter')}
+                    checked={!!shadow.enabled}
+                    onChange={(enabled) => update('enabled', enabled)}
+                />
+                <Typography.Title heading={6} style={{ margin: 8 }}>
+                    {shadow.enabled ? __('Enabled', 'plugin-starter') : __('Disabled', 'plugin-starter')}
+                </Typography.Title>
+            </Space>
             {shadow.enabled && (
-                <>
-                    <Space vertical align='start' className='w-full'>
-                        <UnitControl
-                            label={__('Horizontal Offset (px)', 'plugin-starter')}
-                            onChange={(x) => update('x', x)}
-                            value={shadow.x}
-                            units={units}
-                            className="w-full"
+                <Space vertical align='start' className='w-full'>
+                    <UnitControl
+                        label={__('Horizontal Offset (px)', 'plugin-starter')}
+                        onChange={(x) => update('x', x)}
+                        value={shadow.x}
+                        units={units}
+                        className="w-full"
+                    />
+                    <UnitControl
+                        label={__('Vertical Offset (px)', 'plugin-starter')}
+                        onChange={(y) => update('y', y)}
+                        value={shadow.y}
+                        units={units}
+                        className="w-full"
+                    />
+                    <UnitControl
+                        label={__('Blur (px)', 'plugin-starter')}
+                        onChange={(blur) => update('blur', blur)}
+                        value={shadow.blur}
+                        units={units}
+                        className="w-full"
+                    />
+                    <UnitControl
+                        label={__('Spread (px)', 'plugin-starter')}
+                        onChange={(spread) => update('spread', spread)}
+                        value={shadow.spread}
+                        units={units}
+                        className="w-full"
+                    />
+                    <ColorPickerControl
+                        defaultValue={shadow.color || "#000000"}
+                        handleChange={(color) => update('color', color)}
+                        mode='color'
+                        label={__('Shadow Color', 'plugin-starter')}
+                        className="w-full"
+                    /> 
+                    <div>
+                        <label className='font-semibold block'><Typography.Text>{__('Inset', 'plugin-starter')}</Typography.Text></label>
+                        <Switch 
+                            aria-label={__('Inset', 'plugin-starter')}
+                            checked={!!shadow.inset}
+                            onChange={(enabled) => update('inset', enabled)}
                         />
-                        <UnitControl
-                            label={__('Vertical Offset (px)', 'plugin-starter')}
-                            onChange={(y) => update('y', y)}
-                            value={shadow.y}
-                            units={units}
-                            className="w-full"
-                        />
-                        <UnitControl
-                            label={__('Blur (px)', 'plugin-starter')}
-                            onChange={(blur) => update('blur', blur)}
-                            value={shadow.blur}
-                            units={units}
-                            className="w-full"
-                        />
-                        <UnitControl
-                            label={__('Spread (px)', 'plugin-starter')}
-                            onChange={(spread) => update('spread', spread)}
-                            value={shadow.spread}
-                            units={units}
-                            className="w-full"
-                        />
-                        <ColorPickerControl
-                            defaultValue={shadow.color || "#000000"}
-                            handleChange={(color) => update('color', color)}
-                            mode='color'
-                            label={__('Shadow Color', 'plugin-starter')}
-                            className="w-full"
-                        /> 
-                        <div>
-                            <label className='font-semibold block'><Typography.Text>{__('Inset', 'plugin-starter')}</Typography.Text></label>
-                            <Switch 
-                                aria-label={__('Inset', 'plugin-starter')}
-                                checked={!!shadow.inset}
-                                onChange={(enabled) => update('inset', enabled)}
-                            />
-                        </div>
-                    </Space>
-                </>
+                    </div>
+                </Space>
             )}
         </div>
     );
--- src/components/TextShadowControl/TextShadowControl.jsx
+++ src/components/TextShadowControl/TextShadowControl.jsx
@@ -1,13 +1,7 @@
 import { __ } from '@wordpress/i18n';
-import {
-    PanelBody,
-    RangeControl,
-    ColorPalette,
-    ToggleControl,
-    __experimentalUnitControl as UnitControl, 
-} from '@wordpress/components';
 import {useState} from 'react';
-import ColorPickerControl from '../ColorPickerControl/ColorPickerControl';
+import {ColorPickerControl, UnitControl} from '../../components';
+import { Switch, Space, Typography } from '@douyinfe/semi-ui';
 const units = [
     { value: 'px', label: 'px' },
     // { value: '%', label: '%' },
@@ -26,67 +20,49 @@ const TextShadowControl = ({ value = {}, onChange, className='' }) => {
 
     return (
         <div className={`box-shadow-wrapper ${className}`}>
-            <div className="d-flex justify-content-end mb-2">                
-                <ToggleControl
-                    label={__('Enable Text Shadow', 'plugin-starter')}
+
+            <Space align='center'>
+                <Switch 
+                    aria-label={__('Enable Text Shadow', 'plugin-starter')}
                     checked={!!shadow.enabled}
                     onChange={(enabled) => update('enabled', enabled)}
                 />
-            </div> 
+                <Typography.Title heading={6} style={{ margin: 8 }}>
+                    {shadow.enabled ? __('Enabled', 'plugin-starter') : __('Disabled', 'plugin-starter')}
+                </Typography.Title>
+            </Space>
             
             {shadow.enabled && (
-                <>
-                    <div className="row">
-                        <div className="col-6">
-                            <UnitControl
-                                __next40pxDefaultSize 
-                                label={__('Horizontal Offset (px)', 'plugin-starter')}
-                                value={shadow.x}
-                                onChange={(x) => update('x', x)}
-                                min={-50}
-                                max={50}
-                                units={units}
-                                className='mb-2'
-                            />
-                        </div>
-                        <div className="col-6">
-                            <UnitControl
-                                __next40pxDefaultSize 
-                                label={__('Vertical Offset (px)', 'plugin-starter')}
-                                value={shadow.y}
-                                onChange={(y) => update('y', y)}
-                                min={-50}
-                                max={50}
-                                units={units}
-                                className='mb-2'
-                            />
-                        </div>
-                        <div className="col-6">
-                            <UnitControl
-                                __next40pxDefaultSize 
-                                label={__('Blur (px)', 'plugin-starter')}
-                                value={shadow.blur}
-                                onChange={(blur) => update('blur', blur)}
-                                min={0}
-                                max={100}
-                                units={units}
-                                className='mb-2'
-                            />
-                        </div>
-                        <div className="col-6">
-                            <div className="">
-                                <label className="form-label">{__('color', 'plugin-starter')}</label>
-                                <ColorPickerControl
-                                    defaultValue={shadow.color || "#000000"}
-                                    handleChange={(color) => update('color', color)}
-                                    mode='color'
-                                    label={__('Shadow Color', 'plugin-starter')}
-                                /> 
-                            </div>
-                        </div>
-                    </div>
-                    
-                </>
+                <Space vertical align='start' className='w-full'>
+                    <UnitControl
+                        label={__('Horizontal Offset (px)', 'plugin-starter')}
+                        onChange={(x) => update('x', x)}
+                        value={shadow.x}
+                        units={units}
+                        className='mb-2'
+                    />
+                    <UnitControl
+                        label={__('Vertical Offset (px)', 'plugin-starter')}
+                        onChange={(y) => update('y', y)}
+                        value={shadow.y}
+                        units={units}
+                        className='mb-2'
+                    />
+                    <UnitControl
+                        label={__('Blur (px)', 'plugin-starter')}
+                        onChange={(blur) => update('blur', blur)}
+                        value={shadow.blur}
+                        units={units}
+                        className='mb-2'
+                    />
+                    <ColorPickerControl
+                        defaultValue={shadow.color || "#000000"}
+                        handleChange={(color) => update('color', color)}
+                        mode='color'
+                        label={__('Shadow Color', 'plugin-starter')}
+                        className="w-full"
+                    />                     
+                </Space>
             )}
         </div>
     );
