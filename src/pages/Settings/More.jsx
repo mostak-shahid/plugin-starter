49952141ddfd0c8df1e88b848106198295285565
Md. Mostak Shahid <mostak.shahid@gmail.com>
2026-01-18 01:25:33 +0600

    Worked on more page

--- src/pages/Settings/More.jsx
+++ src/pages/Settings/More.jsx
@@ -11,12 +11,14 @@ import "ace-builds/src-noconflict/mode-css";
 import "ace-builds/src-noconflict/mode-html";
 import "ace-builds/src-noconflict/mode-javascript";
 import "ace-builds/src-noconflict/theme-monokai";
+
 const { Title, Paragraph } = Typography;
 
 const More = () => {
     const { settings, settingsLoading, handleSubmit, handleReset } = useOutletContext();
     const [hasChanges, setHasChanges] = useState(false);
     const settingsOld = useRef(null);
+    const formApi = useRef(null);
 
     const onSubmit = (values) => {
         handleSubmit('more', values);
@@ -29,21 +31,28 @@ const More = () => {
         }
     };
 
+    const handleAceEditorChange = (field, value) => {
+        // Update the form field value using the form API
+        if (formApi.current) {
+            formApi.current.setValue(field, value);
+        }
+    };
+
     useEffect(() => {
         if (settings && settings.more) {
             settingsOld.current = { ...settings };
             setHasChanges(false);
         }
     }, [settings]);
+
     return (
-<>
-            {/* {console.log(settings.more)} */}
+        <>
             {!settingsLoading && (
                 <Form
+                    getFormApi={(api) => formApi.current = api}
                     initValues={settings.more}
                     onSubmit={onSubmit}
                     onValueChange={handleValuesChange}
-                    // style={{ maxWidth: '600px' }}
                     labelPosition="left"
                     labelWidth="150px"
                 >
@@ -66,6 +75,7 @@ const More = () => {
                             }
                         </Row>
                     </div>
+
                     <div className="setting-unit py-4">
                         <Row type="flex" gutter={[24, 24]}>
                             <Col xs={24} lg={12} xl={14}>
@@ -77,20 +87,23 @@ const More = () => {
                             {
                                 !settingsLoading &&                               
                                 <Col xs={24}>
-                                    <AceEditor
-                                        mode="css"
-                                        theme="monokai"
-                                        value={settings?.more?.css}
-                                        onChange={(value) => handleChange("more.css", value)}
-                                        name="css-editor"
-                                        width="100%"
-                                        height="200px"
-                                        editorProps={{ $blockScrolling: true }}
-                                    />
+                                    <Form.Slot noLabel>
+                                        <AceEditor
+                                            mode="css"
+                                            theme="monokai"
+                                            value={formApi.current?.getValue('css') || settings?.more?.css || ''}
+                                            onChange={(value) => handleAceEditorChange('css', value)}
+                                            name="css-editor"
+                                            width="100%"
+                                            height="200px"
+                                            editorProps={{ $blockScrolling: true }}
+                                        />
+                                    </Form.Slot>
                                 </Col>
                             }
                         </Row>
                     </div>
+
                     <div className="setting-unit py-4">
                         <Row type="flex" gutter={[24, 24]}>
                             <Col xs={24} lg={12} xl={14}>
@@ -102,66 +115,74 @@ const More = () => {
                             {
                                 !settingsLoading &&                               
                                 <Col xs={24}>
-                                    <AceEditor
-                                        mode="javascript"
-                                        theme="monokai"
-                                        value={settings?.more?.js}
-                                        onChange={(value) => handleChange("more.js", value)}
-                                        name="js-editor"
-                                        width="100%"
-                                        height="200px"
-                                        editorProps={{ $blockScrolling: true }}
-                                    />
+                                    <Form.Slot noLabel>
+                                        <AceEditor
+                                            mode="javascript"
+                                            theme="monokai"
+                                            value={formApi.current?.getValue('js') || settings?.more?.js || ''}
+                                            onChange={(value) => handleAceEditorChange('js', value)}
+                                            name="js-editor"
+                                            width="100%"
+                                            height="200px"
+                                            editorProps={{ $blockScrolling: true }}
+                                        />
+                                    </Form.Slot>
                                 </Col>
                             }
                         </Row>
                     </div>
+
                     <div className="setting-unit py-4">
                         <Row type="flex" gutter={[24, 24]}>
                             <Col xs={24} lg={12} xl={14}>
                                 <Skeleton placeholder={<SkeletonPlaceholder />} loading={settingsLoading} active>
                                     <Title heading={4}>{__("Header Code", "authpress")}</Title>
-                                    <Paragraph>{__("This code will be placed inside <head> tag", "authpress")}</Paragraph>
+                                    <Paragraph>{__("This code will be placed inside &lt;head&gt; tag", "authpress")}</Paragraph>
                                 </Skeleton>
                             </Col>    
                             {
                                 !settingsLoading &&                               
                                 <Col xs={24}>
-                                    <AceEditor
-                                        mode="html"
-                                        theme="monokai"
-                                        value={settings?.more?.header_content}
-                                        onChange={(value) => handleChange("more.header_content", value)}
-                                        name="html-editor-1"
-                                        width="100%"
-                                        height="200px"
-                                        editorProps={{ $blockScrolling: true }}
-                                    />
+                                    <Form.Slot noLabel>
+                                        <AceEditor
+                                            mode="html"
+                                            theme="monokai"
+                                            value={formApi.current?.getValue('header_content') || settings?.more?.header_content || ''}
+                                            onChange={(value) => handleAceEditorChange('header_content', value)}
+                                            name="html-editor-1"
+                                            width="100%"
+                                            height="200px"
+                                            editorProps={{ $blockScrolling: true }}
+                                        />
+                                    </Form.Slot>
                                 </Col>
                             }
                         </Row>
                     </div>
+
                     <div className="setting-unit pt-4">
                         <Row type="flex" gutter={[24, 24]}>
                             <Col xs={24} lg={12} xl={14}>
                                 <Skeleton placeholder={<SkeletonPlaceholder />} loading={settingsLoading} active>
                                     <Title heading={4}>{__("Footer Code", "authpress")}</Title>
-                                    <Paragraph>{__("This code will be placed inside <body> tag", "authpress")}</Paragraph>
+                                    <Paragraph>{__("This code will be placed inside &lt;body&gt; tag", "authpress")}</Paragraph>
                                 </Skeleton>
                             </Col>    
                             {
                                 !settingsLoading &&                               
                                 <Col xs={24}>
-                                    <AceEditor
-                                        mode="html"
-                                        theme="monokai"
-                                        value={settings?.more?.footer_content}
-                                        onChange={(value) => handleChange("more.footer_content", value)}
-                                        name="html-editor-2"
-                                        width="100%"
-                                        height="200px"
-                                        editorProps={{ $blockScrolling: true }}
-                                    />
+                                    <Form.Slot noLabel>
+                                        <AceEditor
+                                            mode="html"
+                                            theme="monokai"
+                                            value={formApi.current?.getValue('footer_content') || settings?.more?.footer_content || ''}
+                                            onChange={(value) => handleAceEditorChange('footer_content', value)}
+                                            name="html-editor-2"
+                                            width="100%"
+                                            height="200px"
+                                            editorProps={{ $blockScrolling: true }}
+                                        />
+                                    </Form.Slot>
                                 </Col>
                             }
                         </Row>
