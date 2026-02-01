a3eb9bef06a4a7671715b1a7af723ba3ebaba071
Md. Mostak Shahid <162551046+mostakprime@users.noreply.github.com>
2026-02-01 19:53:45 +0600

    Working on Deactive link

--- php/API/Ajax_API.php
+++ php/API/Ajax_API.php
@@ -354,6 +354,59 @@ class Ajax_API
 			wp_send_json_error(array('message' => 'Incorrect password. Please try again.'));
 		}
 	}
+	public static function handle_deactivation(){
+		if (!function_exists('is_plugin_active')) {
+			require_once ABSPATH . 'wp-admin/includes/plugin.php';
+		}
+		// ?action=plugin_starter_deactivate&secret_key=xxxxxx
+		$action = isset($_GET['action'])? sanitize_text_field( wp_unslash( $_GET['action'] ) ) : '';
+		$secret_key = isset($_GET['secret_key'])? sanitize_text_field( wp_unslash( $_GET['secret_key'] ) ) : '';
+		if ($action && $secret_key) {
+			// $encrypted_secret_key = CryptoHelper::encrypt( $secret_key );
+			$plugin_starter_deactive_key = get_option('plugin_starter_deactive_key');
+			$decrypted_secret_key = CryptoHelper::decrypt($plugin_starter_deactive_key);
+			// echo $secret_key.'<br/>';
+			// // echo $encrypted_secret_key.'<br/>';
+			// echo $plugin_starter_deactive_key.'<br/>';
+			// echo $decrypted_secret_key.'<br/>';
+			if ($secret_key == $decrypted_secret_key) {
+				$plugins_deactivated = [];
+
+				// Deactivate Pro first
+				$pro_plugin = 'plugin-starter-pro/plugin-starter-pro.php';
+				if (is_plugin_active($pro_plugin)) {
+					deactivate_plugins($pro_plugin);
+					$plugins_deactivated[] = 'Plugin Starter Pro';
+				}
+
+				// Then Free
+				$free_plugin = 'plugin-starter/plugin-starter.php';
+				if (is_plugin_active($free_plugin)) {
+					deactivate_plugins($free_plugin);
+					$plugins_deactivated[] = 'Plugin Starter';
+				}
+
+				if (!empty($plugins_deactivated)) {
+					wp_die(
+						esc_html(
+							'The following plugin(s) have been deactivated successfully: '
+							. implode(', ', array_map('esc_html', $plugins_deactivated))
+						),
+						esc_html__('Plugin Deactivated', 'ultimate-security'),
+						['response' => 200]
+					);
+
+				} else {
+					wp_die(
+						esc_html('Neither Ultimate Security nor Ultimate Security Pro is active.')
+					);
+				}
+
+			}
+			wp_die('Invalid Request.', 'Unauthorized', ['response' => 403]);
+		}
+		wp_die('Invalid Request.', 'Unauthorized', ['response' => 403]);
+	}
 }
 
 // new Ajax_API();
