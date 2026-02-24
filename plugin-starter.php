9a5d99ffad3e6712431f6369b553b17d0a162e8f
Md. Mostak Shahid <mostak.shahid@gmail.com>
2026-02-25 01:06:52 +0600

    Worked on activation hook

--- plugin-starter.php
+++ plugin-starter.php
@@ -19,8 +19,29 @@ define('PLUGIN_STARTER_PATH', plugin_dir_path(__FILE__));
 define('PLUGIN_STARTER_URL', plugin_dir_url(__FILE__));
 define('PLUGIN_STARTER_MAIN_FILE', __FILE__);
 
-require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/plugin-starter-functions.php';
+require_once PLUGIN_STARTER_PATH . '/vendor/autoload.php';
+require_once PLUGIN_STARTER_PATH . '/plugin-starter-functions.php';
+
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in src/Core/Activator.php
+ */
+function plugin_starter_activate()
+{
+	\MosPress\PluginStarter\Core\Activator::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in src/Core/Deactivator.php
+ */
+function plugin_starter_deactivate()
+{
+	\MosPress\PluginStarter\Core\Deactivator::deactivate();
+}
+
+register_activation_hook(__FILE__, 'plugin_starter_activate');
+register_deactivation_hook(__FILE__, 'plugin_starter_deactivate');
 
 use MosPress\PluginStarter\Plugin;
 
