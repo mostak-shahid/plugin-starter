cb64e1ef839bd902e30548e3b4d2cb1372f91074
Md. Mostak Shahid <mostak.shahid@gmail.com>
2026-03-24 23:51:30 +0600

    added wp-scripts functionality

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const defaultConfig = require("@wordpress/scripts/config/webpack.config");
+const { ModuleFederationPlugin } = require("webpack").container;
+
+module.exports = {
+	...defaultConfig,
+
+	entry: {
+		app: path.resolve(__dirname, "assets/src/index.js"),
+		profile: path.resolve(__dirname, "assets/src/profile/profile.js"),
+	},
+
+	output: {
+		filename: "[name].js",
+		path: path.resolve(__dirname, "assets/build"),
+		publicPath: "auto",
+	},
+
+	module: {
+		...defaultConfig.module,
+		rules: [
+			...defaultConfig.module.rules,
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env", "@babel/preset-react"],
+					},
+				},
+			},
+		],
+	},
+
+	plugins: [
+		...defaultConfig.plugins,
+		new ModuleFederationPlugin({
+			name: "pluginstarter",
+			remotes: {
+				pluginstarterpro: `pluginstarterpro@/wp-content/plugins/plugin-starter-pro/build/pluginstarterprocomponents.js`,
+			},
+			shared: {
+				react: { singleton: true, requiredVersion: false, eager: false },
+				"react-dom": { singleton: true, requiredVersion: false, eager: false },
+			},
+		}),
+	],
+};
