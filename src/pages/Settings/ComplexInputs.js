0bb2d8b00a8b06915ef40b01ed324c14abde376c
Md. Mostak Shahid <mostak.shahid@gmail.com>
2026-07-30 03:17:05 +0600

    Update ComplexInputs.js

--- src/pages/Settings/ComplexInputs.js
+++ src/pages/Settings/ComplexInputs.js
@@ -215,7 +215,7 @@ const ComplexInputs = () => {
                                     enabler: true,
                                 }}
                                 fields={[
-                                    { type: "input", name: "title", placeholder: "Address 1", className: "input-field", label: "Address 1" },
+                                    { type: "input", name: "title", placeholder: "Address 1", className: "input-field", label: "Address 1", attributes:{'data-url': 'someurl', 'list': "browsers"}  },
                                     { type: "textarea", name: "note", placeholder: "Note", className: "textarea-field", label: "Note" },
                                     { type: "checkbox", name: "enable", placeholder: "Enable", className: "checkbox-field", label: "Enable" },
                                     { type: "radio", name: "gender", className: "radio-field", label: "Gender", options: [{ key: "male", value: "Male" }, { key: "female", value: "Female" }] },
